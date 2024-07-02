6e544988ba1c69040cbf4f075bd7bac68382b727
Thiago Rodrigues <thiagothogs@icloud.com>
2024-07-02 09:53:11 -0300

    feat: added tables mapping

--- src/main/java/codesumn/com/marketplace_backend/app/models/UserModel.java
+++ src/main/java/codesumn/com/marketplace_backend/app/models/UserModel.java
@@ -15,11 +15,22 @@ public class UserModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID")
     private UUID id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "role", nullable = false)
     private String role;
 
     public UserModel() {
